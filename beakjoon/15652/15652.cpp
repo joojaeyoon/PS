bc32d4255744a65a96e625e2fc8f467944ee88a9
wow1548 <wow1548@naver.com>
2020-05-11 15:54:31 +0900

    N&M4

---
+++ beakjoon/15652/15652.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+int N, M;
+int arr[] = {
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+};
+
+void print_set(vector<int> &selected, int prev, int toPick)
+{
+    if (toPick == 0)
+    {
+        for (int i = 0; i < selected.size(); i++)
+            cout << selected[i] << " ";
+        cout << "\n";
+        return;
+    }
+
+    for (int i = prev; i < N; i++)
+    {
+        selected.push_back(arr[i]);
+        print_set(selected, i, toPick - 1);
+        selected.pop_back();
+    }
+}
+int main()
+{
+    cin >> N >> M;
+    vector<int> v;
+
+    print_set(v, 0, M);
+
+    return 0;
+}
