8862be2cb7c7636f23032c635d50bded280ae58c
wow1548 <wow1548@naver.com>
2020-05-13 09:51:58 +0900

    Arrange sovled problems

--- beakjoon/file.py
+++ beakjoon/file.py
@@ -6,9 +6,18 @@ filelist = os.listdir()
 
 for f in filelist:
     if f[0].isdigit() and os.path.isfile(f):
-        name = f.split(".")[0]
+        number = f.split(".")[0]
+        min_range = int(number)//1000*1000
+        max_range = min_range+1000
 
-        if not os.path.exists(name):
-            os.mkdir(name)
+        f_name = str(min_range)+"-"+str(max_range)
 
-        os.rename(f, name+"/"+f)
+        path = f_name+"/"+number+"/"+f
+
+        if not os.path.exists(f_name):
+            os.mkdir(f_name)
+
+        if not os.path.exists(f_name+"/"+number):
+            os.mkdir(f_name+"/"+number)
+
+        os.rename(f, path)
