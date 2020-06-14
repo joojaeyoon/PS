9f751714cf38669c3fed76dbc91320dda9ef1076
wow1548 <wow1548@naver.com>
2020-06-14 19:15:57 +0900

    camera

---
+++ Programmers/camera.py
@@ -0,0 +1,18 @@
+def solution(routes):
+    answer = 1
+    routes.sort()
+
+    tmp = routes[0][1]
+    routes.pop(0)
+
+    for r in routes:
+        if r[0] <= tmp:
+            tmp = min(tmp, r[1])
+        else:
+            tmp = r[1]
+            answer += 1
+
+    return answer
+
+
+print(solution([[-20, 15], [-14, -5], [-18, -13], [-5, -3]]))
