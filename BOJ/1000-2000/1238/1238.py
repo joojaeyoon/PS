46f9a6ebcb5929726b127b240f69b64c16160af2
Joo <wow1548@naver.com>
2020-10-13 16:47:11 +0900

    1238

--- BOJ/1000-2000/1238/1238.py
+++ BOJ/1000-2000/1238/1238.py
@@ -4,32 +4,40 @@ from heapq import heappop, heappush
 N, M, X = map(int, input().split())
 
 ways = [[] for _ in range(N+1)]
-dists = [[sys.maxsize for __ in range(N+1)] for _ in range(N+1)]
+ways_back = [[] for _ in range(N+1)]
+dist1 = [sys.maxsize for _ in range(N+1)]
+dist2 = [sys.maxsize for _ in range(N+1)]
 
 for _ in range(M):
     a, b, c = map(int, sys.stdin.readline().split())
 
     ways[a].append((c, b))
+    ways_back[b].append((c, a))
 
-for A in range(1, N+1):
-    dists[A][A] = 0
+
+def dijkstra(ways, A, dist):
+    dist[A] = 0
     heap = []
 
     for cost, n in ways[A]:
-        dists[A][n] = cost
+        dist[n] = cost
         heappush(heap, (cost, n))
 
     while heap:
         cost, cur = heappop(heap)
 
         for c, nxt in ways[cur]:
-            new_cost = dists[A][cur]+c
+            new_cost = dist[cur]+c
 
-            if new_cost < dists[A][nxt]:
-                dists[A][nxt] = min(dists[A][nxt], new_cost)
+            if new_cost < dist[nxt]:
+                dist[nxt] = new_cost
                 heappush(heap, (new_cost, nxt))
-answer = 0
 
+
+dijkstra(ways, X, dist1)
+dijkstra(ways_back, X, dist2)
+
+answer = 0
 for i in range(1, N+1):
-    answer = max(answer, dists[i][X]+dists[X][i])
+    answer = max(answer, dist1[i]+dist2[i])
 print(answer)
